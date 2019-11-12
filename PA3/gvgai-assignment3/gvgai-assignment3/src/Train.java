4fe1e8c07a8c4ca2e97a3f1472227f18b3bf93f2
181220063-Xue Zhenghai <xuezh@smail.nju.edu.cn>
2019-11-12 11:53:19 +0800

    before starting pa3

--- PA3/gvgai-assignment3/gvgai-assignment3/src/Train.java
+++ PA3/gvgai-assignment3/gvgai-assignment3/src/Train.java
@@ -8,11 +8,12 @@ import tools.Recorder;
 public class Train
 {
     // ! 需要将weka.jar拷贝到/usr/lib/jvm/java-8-openjdk-amd64/jre/lib/ext/
+    // ! 运行weka方法:java -jar weka.jar
     public static void main(String[] args) throws Exception
     {
         Recorder recorder  = new Recorder("AliensRecorder");
         
         Game.setRecorder(recorder);
-        ArcadeMachine.playOneGame( "examples/gridphysics/aliens.txt", "examples/gridphysics/aliens_lvl0.txt", null, new Random().nextInt());
+        ArcadeMachine.playOneGame( "examples/gridphysics/aliens.txt", "examples/gridphysics/aliens_lvl4.txt", null, new Random().nextInt());
     }
 }
